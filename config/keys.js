e1c2cbeca1a5c435df63fea901990c0c3f3e48e6
joshimcse <joshimcse@gmail.com>
2019-04-14 03:57:01 +0600

    connected to mongodb database

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+/**
+ * keys.js
+ * 
+ * @description :: 
+ * @author      :: Joshim Uddin
+ */
+
+module.exports = {
+  mongoURI: 'mongodb://joshimcse:jcse24@ds211865.mlab.com:11865/devconnect'
+}
--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  */
 
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 // set up express app
@@ -32,5 +33,21 @@ app.use(morgan('dev'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Magic happens on port ${port}`);
+
+  // Load DB
+  const db = require('./config/keys').mongoURI;
+
+  // mongoDB configuration
+  mongoose.connect(
+    db, { useNewUrlParser: true }, (err, res) => {
+      if (err) console.error(err);
+      else console.log('Connected to Database');
+    }
+  );
+});
+
+//Testing Routes
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: 'routes work successfully' });
 });
 
